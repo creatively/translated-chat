5f5b0551c1a3fbe9df3aea751281a0ced55259ed
Chris Charles <chris@creatively.co.uk>
2022-04-20 19:34:37 +0100

    small refactor of script.js

--- public/js/script.js
+++ public/js/script.js
@@ -9,63 +9,9 @@ const messageInput = document.getElementById('message-input')
 const c = txt => console.log(txt);
 const ct = obj => console.table(obj);
 const add = text => setTimeout(() => {messageContainer.innerHTML = messageContainer.innerHTML + `<i>${text}</i><br>`}, 500)
-const isCssSupported = property => property in document.body.style
-
-const shiftDownMessageContainerViewport = () => {
-  const messageContainerTotalHeight = document.getElementById('message-container').clientHeight
-  window.scrollTo(0, messageContainerTotalHeight)
-}
-
-// SET UP FUNCTIONS
-window.addEventListener('resize', shiftDownMessageContainerViewport)
-document.getElementById('send-button').addEventListener('click', shiftDownMessageContainerViewport)
 
 
-      // NB. On iOS, "window.resize" & possily "window.innerHeight" is affected by :
-      //    - url bar show
-      //    - portarit/landscape
-      //    but not keyboard show (possibly just not triggering window.resize)
-
-/*
-  if (navigator.onLine) {   // true/false
-  window.addEventListener('online', function(e) {
-  window.addEventListener('offline', function(e) {
-
-
-  document.addEventListener("visibilitychange", (event) => {
-    if (document.visibilityState == "visible") {
-
-
-  window.addEventListener('beforeunload', function (e) {
-      e.preventDefault();
-      e.returnValue = '';
-  });
-
-
-
-*/
-
-/*
-OBJECT FOR CONNECTION HANDLING
-socket.emit('user-status-change', roomName, status)
-  statuses = [[ back-online , tab-switched-back , tab-closing ]]
-
-
-SERVER.JS
-  usersConnection.on('disconnect', () => {
-    getUserRooms(usersConnection).forEach(roomName => {
-      const leaversName = rooms[roomName].users[usersConnection.id].name
-      ... io.in(roomName).emit('user-disconnected', leaversName, latestActiveUsers)
-      ----
-
-
-
-
-
-*/
-
-
-// Name & Language Inout Form
+// Name & Language Input Form
 const handleModalSubmit = e => {
   e.preventDefault();
   const name = document.querySelector('#input-name').value;
@@ -88,17 +34,17 @@ headingPrivacyPolicy.addEventListener('click', toggleMenuItem)
 headingCarbonUsage.addEventListener('click', toggleMenuItem)
 
 document.body.classList.add('bg')
-document.querySelectorAll('.icon-copy').forEach(icon => icon.addEventListener('click', copyToClipboard))
+document.querySelectorAll('.icon-copy').forEach(icon => icon.addEventListener('click', copyUrlToClipboard))
 
 function toggleMenu() {
-    if (menuTitles.classList.contains('open')) {
-      menuTitles.classList.remove('open')
-      overlay.classList.remove('on')
-    } else {
-      menuTitles.classList.add('open')
-      overlay.classList.add('on')
-    }
-    return false
+  if (menuTitles.classList.contains('open')) {
+    menuTitles.classList.remove('open')
+    overlay.classList.remove('on')
+  } else {
+    menuTitles.classList.add('open')
+    overlay.classList.add('on')
+  }
+  return false
 }
 
 function toggleMenuItem(e) {
@@ -139,11 +85,6 @@ const nameAndLanguageForm = document.querySelector('.name-and-language-form') ||
 nameAndLanguageForm ? nameAndLanguageForm.addEventListener('submit', handleModalSubmit) : null;
 
 
-
-
-
-
-
 // Socket handling
 function joinNewUser(name, language) {
 
@@ -160,9 +101,6 @@ function joinNewUser(name, language) {
     socket.emit('send-chat-message', roomName, message)
     messageInput.value = ''
   })
-
-
-
 }
 
 // receive chat message
@@ -184,7 +122,7 @@ socket.on('info-message-only-user', data => {
   appendMessage({
     textInfo: 
       `${data.message}, - send this url to invite others<br>
-      <image class="icon-copy" src="./images/icon-copy.png" onclick="copyToClipboard()" />`
+      <image class="icon-copy" src="./images/icon-copy.png" onclick="copyUrlToClipboard()" />`
   })
 })
 
@@ -203,7 +141,7 @@ socket.on('user-disconnected', (name, activeUsers) => {
 })
 
 // Initialise starting form
-const populateLanguagesList = () => {
+const populateLanguagesSelectLanguageList = () => {
   const dropdownSelectLanguage = document.getElementById('select-language')
   for (languageCode in supportedLanguages) {
     let newOption = new Option(supportedLanguages[languageCode], languageCode);
@@ -211,13 +149,17 @@ const populateLanguagesList = () => {
   }
 }
 
-document.body.onload = () => {
-  populateLanguagesList()
+const setLanguagesSelectToTheSystemLanguage = () => {
   document.getElementById('select-language').value = navigator.language.substring(0,2)
+}
+
+document.body.onload = () => {
+  populateLanguagesSelectLanguageList()
+  setLanguagesSelectToTheSystemLanguage()
   document.getElementById('input-name').focus()
 }
 
-function copyToClipboard(e) {
+function copyUrlToClipboard(e) {
   if (navigator.clipboard && navigator.clipboard.writeText) {
     navigator.clipboard.writeText(location.href)
     document.querySelectorAll('.icon-copy').forEach(item => {
@@ -225,7 +167,6 @@ function copyToClipboard(e) {
     })
   }
 }
-
 
 // Display message in the messages container
 function appendMessage(textObject) {
